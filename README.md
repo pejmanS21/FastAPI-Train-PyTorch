fe737cd78d0b3ea90acf8a39d86fb76754f94fd5
pezhmansamadi21 <pezhmansamadi21@gmail.com>
2022-07-06 17:55:51 +0430

    HTML Settings
    change commands to run model via HTML file

--- README.md
+++ README.md
@@ -2,4 +2,14 @@
 
 >***incomplete***
 
+<<<<<<< HEAD
+```bash
+% chmod a+x run.sh
+% ./run.sh
+```
+
+- [ ] result.html
+- [ ] apply style
+=======
 - [ ] check Pytorch MPS
+>>>>>>> d01bb5752ddea47c1b1167e5eddddecd7cc56b41
---
+++ run.sh
@@ -0,0 +1,6 @@
+pip install virtualenv
+virtualenv venv
+source venv/bin/activate
+pip install -r requirements.txt
+# uvicorn main:app --reload --port 8000 --host 0.0.0.0
+uvicorn app:app --reload --port 8000 --host 0.0.0.0
